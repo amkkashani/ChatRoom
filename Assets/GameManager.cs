fb58edf1d217258ee3d26b11105e783eae9b0b59
A.H.kashani <amkkashani@gmail.com>
2021-08-14 10:46:13 +0430

    goServer updated

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -9,17 +11,17 @@ public class GameManager : MonoBehaviour
     public Transform whereMustAdd;
     public GameObject lineElement;
 
-    
+    private int targetNumberMassage = 0;
+    private string myName = "oops";
+
+    private List<GameObject> created;
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(GetText());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        created = new List<GameObject>();
+//        StartCoroutine(updatIndex(2));
+//        sendButton("hello guys");
     }
 
     public void makeNewRow(string text)
@@ -27,23 +29,145 @@ public class GameManager : MonoBehaviour
         GameObject newRow = Instantiate(lineElement, whereMustAdd, true);
         Text textfield = newRow.GetComponent<Text>();
         textfield.text = text;
+        created.Add(newRow);
     }
-    
-    IEnumerator GetText() {
-        UnityWebRequest www = UnityWebRequest.Get("http://localhost:8005/see");
+
+    IEnumerator GetAll(string name)
+    {
+        destroyAllMassages();
+        myName = name;
+
+        UnityWebRequest www = UnityWebRequest.Get("http://localhost:8005/all");
         yield return www.SendWebRequest();
- 
-        if(www.isNetworkError || www.isHttpError) {
+
+        if (www.isNetworkError || www.isHttpError)
+        {
             Debug.Log(www.error);
         }
-        else {
+        else
+        {
             // Show results as text
             Debug.Log(www.downloadHandler.text);
- 
+            string json = www.downloadHandler.text;
             // Or retrieve results as binary data
             byte[] results = www.downloadHandler.data;
+
+
+            List<Massage> x = JsonConvert.DeserializeObject<List<Massage>>(json);
+
+            if (x != null)
+            {
+                for (int i = 0; i < x.Count; i++)
+                {
+                    Massage temp = x[i];
+                    makeNewRow(String.Format("[{0}] : \n {1} ", temp.Owner, temp.Text));
+                }
+
+                targetNumberMassage = x.Count;
+            }
+        }
+
+        StartCoroutine(updatIndex());
+    }
+
+
+    IEnumerator updatIndex()
+    {
+        string currentName = myName;
+        while (myName == currentName)
+        {
+            UnityWebRequest www = UnityWebRequest.Get("http://localhost:8005/update?index=" + targetNumberMassage);
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+                string json = www.downloadHandler.text;
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+
+
+                List<Massage> x = JsonConvert.DeserializeObject<List<Massage>>(json);
+
+                if (x != null)
+                {
+                    int mine =  0;
+                    for (int i = 0; i < x.Count; i++)
+                    {
+                        if (x[i].Owner == myName)
+                        {
+                            mine++;
+                            continue;
+                        }
+                        Massage temp = x[i];
+                        makeNewRow(String.Format("[{0}] : \n {1} ", temp.Owner, temp.Text));
+                    }
+
+                    targetNumberMassage += x.Count - mine;
+                }
+            }
+        }
+    }
+
+    public void getAllButton(InputField text)
+    {
+        StartCoroutine(GetAll(text.text));
+    }
+
+
+    public void sendButton(InputField str)
+    {
+        Massage newMassage = new Massage() {Owner = myName, Text = str.text};
+        string sendToServer = JsonConvert.SerializeObject(newMassage);
+        StartCoroutine(send(sendToServer));
+    }
+
+    IEnumerator send(string sendToServer)
+    {
+        UnityWebRequest www = UnityWebRequest.Put("http://localhost:8005/send", sendToServer);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+            string json = www.downloadHandler.text;
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+
+
+            Massage x = JsonConvert.DeserializeObject<Massage>(json);
+
+            if (x != null)
+            {
+                makeNewRow(String.Format("[{0}] : \n {1} ", x.Owner, x.Text));
+                targetNumberMassage++;
+            }
         }
     }
 
 
+    private void destroyAllMassages()
+    {
+        for (int i = 0; i < created.Count; i++)
+        {
+            Destroy(created[i]);
+        }
+    }
+
+
+    public class Massage
+    {
+        public string Owner;
+        public string Text;
+    }
 }
--- goServer/main.go
+++ goServer/main.go
@@ -3,23 +3,101 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
-func dataGet(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "salam kashi hastam")
-	time.Sleep(10 * time.Second)
-	fmt.Fprintf(w, "این هم بقیه اش")
+//structs
+type payam struct {
+	Owner string
+	Text  string
+}
+
+// type Room struct {
+// 	name     string
+// 	massages []Massage
+// }
+
+func update(w http.ResponseWriter, req *http.Request) {
+	index := req.URL.Query()["index"]
+	indexNumber, _ := strconv.Atoi(string(index[0]))
+	fmt.Print(indexNumber)
+
+	var sendList []payam
+
+	for j := 0; j < 14; j++ {
+		if indexNumber <= len(massageList)-1 {
+			for i := indexNumber; i < len(massageList); i++ {
+				sendList = append(sendList, massageList[i])
+			}
+			break
+		} else {
+			time.Sleep(time.Millisecond * 500)
+		}
+	}
+	output, _ := json.Marshal(sendList)
+	fmt.Fprintf(w, string(output))
 }
 
 func dataSend(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "test")
+
+	decoder := json.NewDecoder(req.Body)
+	var t payam
+	err := decoder.Decode(&t)
+
+	if err != nil {
+		panic(err)
+	}
+	log.Println(t.Owner)
+	massageList = append(massageList, t)
+	log.Println(len(massageList))
+
+	output, _ := json.Marshal(t)
+	fmt.Fprint(w, string(output))
 }
 
+func getAllData(w http.ResponseWriter, req *http.Request) {
+
+	test, _ := json.Marshal(massageList)
+	print(string(test))
+	fmt.Fprintf(w, string(test))
+
+}
+
+//variables
+var m map[string]string
+var massageList []payam
+
 func main() {
-	http.HandleFunc("/see", dataGet)
+	// res1D := &response1{
+	// 	Page:   1,
+	// 	Fruits: []string{"apple", "peach", "pear"}}
+	// res1B, _ := json.Marshal(res1D)
+	// fmt.Println(string(res1B))
+
+	// input := &payam{
+	// 	ID:    3,
+	// 	Owner: "ali",
+	// 	Text:  "salam"}
+	// res, _ := json.Marshal(input)
+	// fmt.Println(string(res))
+
+	// var newPayam2 payam
+	// json.Unmarshal([]byte(res), &newPayam2)
+	// println(newPayam2.ID)
+
+	// newMassage := payam{ID: 54, Owner: "hasan", Text: "salam be hame"}
+	// massageList = append(massageList, newMassage)
+	// massageList = append(massageList, newMassage)
+	// fmt.Println(massageList)
+
+	http.HandleFunc("/update", update)
 	http.HandleFunc("/send", dataSend)
+	http.HandleFunc("/all", getAllData)
 	http.ListenAndServe(":8005", nil)
+
 }
